007ae99602b9847a494f1543ec3d3acac57fbee4
ztytotoro <ztytotoro@outlook.com>
2017-09-14 17:12:46 +0800

    update

--- PowerSearch.psm1
+++ PowerSearch.psm1
@@ -1,51 +1,93 @@
 [Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
 function bing {
     #Start-Process microsoft-edge:www.bing.com?q=$url;
-    Start-Process -FilePath ("www.bing.com?q=" + $args);
+    if($args.Length -gt 0){
+        Start-Process -FilePath ("www.bing.com?q=" + $args);
+    }
+    else{
+        Start-Process -FilePath ("www.bing.com" + $args);
+    }
 }
 
 function google {
-    Start-Process -FilePath "chrome.exe" ("www.google.com/search?q=" + ($args -join "+"));
+    if($args.Length -gt 0){
+        Start-Process -FilePath "chrome.exe" ("www.google.com/search?q=" + ($args -join "+"));
+    }
+    else{
+        Start-Process -FilePath "chrome.exe" ("www.google.com/ncr");
+    }
 }
 
 function wiki($query) {
-    Start-Process -FilePath ("www.wikipedia.org/wiki/" + $query);
+    if($query){
+        Start-Process -FilePath ("www.wikipedia.org/wiki/" + $query);
+    }
+    else{
+        Start-Process -FilePath ("www.wikipedia.org");
+    }
 }
 
 function github {
-    if($args.Contains("/")){
-        Start-Process -FilePath ("www.github.com/" + $args);
-    }else{
-        Start-Process -FilePath ("www.github.com/search?q=" + $args);
+    if($args.Length -gt 0){
+        if ($args[0].Contains("/")) {
+            Start-Process -FilePath ("www.github.com/" + $args[0].Replace("/", ""));
+        }
+        else {
+            Start-Process -FilePath ("www.github.com/search?q=" + $args);
+        }
+    }else {
+        Start-Process -FilePath ("www.github.com");
     }
 }
 
 function zhihu {
-    Start-Process -FilePath ("www.zhihu.com/search?q=" + $args);
+    if($args.Length -gt 0){
+        Start-Process -FilePath ("www.zhihu.com/search?q=" + $args);
+    }else{
+        Start-Process -FilePath ("www.zhihu.com");
+    }
 }
 
 function tieba($query) {
-    Start-Process -FilePath ("https://tieba.baidu.com/f?kw=" + $query);
+    if($query){
+        Start-Process -FilePath ("https://tieba.baidu.com/f?kw=" + $query);
+    }else{
+        Start-Process -FilePath ("https://tieba.baidu.com");
+    }
 }
 
 function stack {
-    Start-Process -FilePath ("www.stackoverflow.com/search?q=" + $args);
+    if($args.Length -gt 0){
+        Start-Process -FilePath ("www.stackoverflow.com/search?q=" + $args);
+    }else{
+        Start-Process -FilePath ("www.stackoverflow.com");
+    }
 }
 
 function jd {
-    Start-Process -FilePath ("https://search.jd.com/Search?keyword=" + [System.Web.HttpUtility]::UrlEncode($args) + "&enc=utf-8");
+    if($args.Length -gt 0){
+        Start-Process -FilePath ("https://search.jd.com/Search?keyword=" + [System.Web.HttpUtility]::UrlEncode($args) + "&enc=utf-8");
+    }
+    else{
+        Start-Process -FilePath ("https://wwww.jd.com");
+    }
 }
 
 function douyu($query) {
-    $raw = Get-Content -Encoding UTF8 $HOME/psconfig/douyu.json
-    $rooms = ConvertFrom-Json ([string]$raw)
-    $query = if ($rooms.$query) {
-        $rooms.$query
+    if($query){
+        $raw = Get-Content -Encoding UTF8 $HOME/psconfig/douyu.json
+        $rooms = ConvertFrom-Json ([string]$raw)
+        $query = if ($rooms.$query) {
+            $rooms.$query
+        }
+        else {
+            $query
+        }
+        Start-Process -FilePath ("www.douyu.com/" + $query);
     }
-    else {
-        $query
+    else{
+        Start-Process -FilePath ("www.douyu.com");
     }
-    Start-Process -FilePath ("www.douyu.com/" + $query);
 }
 
 function edge($url) {
